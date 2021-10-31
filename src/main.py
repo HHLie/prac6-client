50fc23b7a38abf54bcf515c7a4ce1a90ee24a01d
LXXHSI007 <lxxhsi007@myuct.ac.za>
2021-10-31 13:25:18 +0200

    some stuff

--- src/main.py
+++ src/main.py
@@ -1,9 +1,80 @@
-from flask import Flask
-app = Flask(__name__)
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-@app.route('/')
-def hello_world():
-    return 'Hello EEE3095S! <br/> This is our webserver <br/> Done by: LXXHSI007 and VBNREE001'
+import time
+import threading
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+# SETUP
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+ADC = AnalogIn(mcp, MCP.P1)
+
+# create an analog input channel on pin 1
+LDR = AnalogIn(mcp, MCP.P2)
+
+# set up button
+button = digitalio.DigitalInOut(board.D26)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
+
+# math to convert temp sensor voltage into *C
+def ConvertTemp(data):
+    temp = data - 0.5
+    temp = temp / 0.01
+    return round(temp, 2)
+
+
+# interval for each new threads, default 10 = 10seconds runtime interval
+interval = 10
+
+
+# for cycling between intervals, for button
+def cycle():
+    global interval
+    if interval == 10:
+        interval = 5
+    elif interval == 5:
+        interval = 1
+    else:
+        interval = 10
+    return interval
+
+
+def print_time_thread():  # thread that prints readings from ADC
+    global ADC, LDR, runtime, interval
+    thread = threading.Timer(interval, print_time_thread)
+    thread.daemon = True  # Daemon threads exit when the program does
+    thread.start()
+    # runtime
+    print((str(round(time.time() - starttime)) + "s").ljust(9, ' '),
+          # temp adc
+          str(ADC.value).ljust(14, ' '),
+          # temp C
+          (str(ConvertTemp(ADC.voltage)) + "C").ljust(9, ' '),
+          # light resistor reading
+          LDR.value)
+
+
+if __name__ == "__main__":
+    print("Runtime   Temp Reading   Temp      Light Reading")
+    starttime = time.time()
+    print_time_thread()
+
+    while True:
+        if button.value == False:
+            time.sleep(0.25)  # software debounce
+            print("Changing intervals to: " + str(cycle()) + "s")
